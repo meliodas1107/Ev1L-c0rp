523dd50fe4e3058c88688c0c9b73d6f5e0853b0d
Devansh Batham <devanshbatham009@gmail.com>
2020-01-22 22:14:07 +0530

    Create kwute.py

---
+++ kwute.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python 
+
+
+'''
+author : Asm0d3us ( Devansh Batham )
+'''
+string = "W]UwSgLA"
+
+hint = []
+for i in string:
+	hint.append(chr((ord(i)^52)))
+print(''.join(hint))
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+https://pastebin.com/{''join(hint)}
+'''
